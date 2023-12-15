f575b4db035c1b2d855a6a89383e3f5490867f08
Elite6277 <hacklinux93@gmail.com>
2023-12-15 19:10:24 +0500

    React_Elements&Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+import { Component } from "react"
+
+const Header = () => {
+  return <h2>Hello Worldddd!</h2>
+}
+
+// const Field = () => {
+//   const holder = "Enter here"
+//   const styledField = {
+//     width: "300px",
+//   }
+//   return <input type="text" placeholder={holder} style={styledField} />
+// }
+
+// Создаем компонент с синтаксисом классов
+class Field extends Component {
+  render() {
+    const holder = "Enter here"
+    const styledField = {
+      width: "300px",
+    }
+
+    return <input type="text" placeholder={holder} style={styledField} />
+  }
+}
+
+function Btn() {
+  const text = "Log in"
+  const logged = false
+  // const res = () => {
+  //   return "Log in"
+  // }
+  // const p = <p>Log in</p>
+  return (
+    <button type="submit" class="button">
+      {logged ? "Enter" : text}
+    </button>
+  )
+}
+
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div className="App">
+      <Header />
+      <Field />
+      <Btn />
+    </div>
+  )
 }
 
 export default App
